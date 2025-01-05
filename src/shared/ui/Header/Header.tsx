e0d1bb289c00e492bd6edb83da9f260733e04d53
parkjunsoo <01junsoo@gmail.com>
2025-01-05 18:05:32 +0900

    feat: body layout

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,4 +1,16 @@
+import css from './Header.module.scss';
+
 interface LayoutHeaderProps {}
 export const LayoutHeader: React.FC<LayoutHeaderProps> = () => {
-  return <header>header</header>;
+  return (
+    <header className={css.layout__header}>
+      <div className={css.layout__header__content}>
+        <h1 className={css.layout__header__title}>Hoooolder</h1>
+        <div className={css.layout__header__actions}>
+          <a href="/login">theme</a>
+          <a href="/register">github</a>
+        </div>
+      </div>
+    </header>
+  );
 };
